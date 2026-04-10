79857a5b421e430cbead1920f50b5e64f1181987
zhenechka1 <drobotyevhen@gmail.com>
2026-04-10 04:08:14 +0300

    Update task.py

--- models/task.py
+++ models/task.py
@@ -12,6 +12,7 @@ class Task:
         status = "Виконано" if self.completed else "Активна"
 //Хардкодити текст інтерфейсу ("Виконано", "Активна") прямо в data-моделі — не дуже хороша практика. Краще винеси цю логіку форматування кудись у хендлери або сервіс відображення. Actually, в нас теж таке, але це ж MVP)
         deadline_text = self.deadline if self.deadline else "не встановлено"
+// Це можна написати коротше і гарніше на пітоні: deadline_text = self.deadline or "не встановлено".
         return (
             f"[{self.task_id}] {self.title}\n"
             f"Дедлайн: {deadline_text}\n"
